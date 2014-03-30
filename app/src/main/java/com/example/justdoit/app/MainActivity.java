7653c65a62cf94d4e89254b6ddd1e6ed35e87ee9
David M <uberl33thax0r@hotmail.com>
2014-03-30 04:41:14 -0400

    initial commit

---
+++ app/src/main/java/com/example/justdoit/app/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.justdoit.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView suggestedActivityTextView;
+    Button suggestActivityButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        suggestedActivityTextView = (TextView) findViewById(R.id.suggestedActivityTextView);
+        suggestActivityButton = (Button) findViewById(R.id.suggestActivityButton);
+
+        // Set suggested activity to something random on load
+        String s = getRandomSuggestedActivity();
+        suggestedActivityTextView.setText(s);
+
+
+        // Stuff specific to initial startup or restoration
+        if( savedInstanceState == null ) {
+            // Just starting
+        } else {
+            // App is being restored
+        }
+
+        setButtonOnClickListeners();
+
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Save instance
+        //outState.putDouble(KEY,value);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setButtonOnClickListeners() {
+        suggestActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set suggested activity to something new
+                String s = getRandomSuggestedActivity();
+                suggestedActivityTextView.setText(s);
+            }
+        });
+    }
+
+    private String getRandomSuggestedActivity() {
+
+        // TEMP for debug.. activities should be pulled from external source (flatfile? dropbox? webservice?)
+        String allActivities[] = {
+                "Go for a walk in the woods",
+                "Read a book",
+                "Work on closet shelf",
+                "Build PC from spare parts",
+                "Play Guitar",
+                "Paint wooden chest",
+                "Draw something from drawme folder",
+                "Dust out computers",
+                "Stretch / do yoga",
+                "Go for a bike ride",
+                "Learn to read/write korean",
+                "Learn to read/write chinese",
+                "Work on paper mache train tunnel",
+                "Make paper mache piggie bank",
+                "Work on scroll for lauren",
+                "Plaster work",
+                "Reschedule mulroy (~May 8th)",
+                "Set up personal website",
+                "Work on demo site for resume",
+                "Work on android app",
+                "Work on iOS app",
+                "Clean room",
+                "Work on python project",
+                "Go take pictures",
+                "Learn a song on guitar",
+                "Learn to compose music on fruityloops",
+                "Learn MIDI",
+                "Work on HTML5 version of this app",
+                "Work on this app"
+        };
+
+        int i = new Random().nextInt(allActivities.length);
+        return allActivities[i];
+    }
+
+}
